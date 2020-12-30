3ee3ff47e0e67e9a4339996cb05e6d3025c62aac
rozek1997 <marek.roskowicz@student.wat.edu.pl>
2020-12-30 03:04:14 +0100

    implemented cross-stack cloudformation reference, allowing to access outputs reference values from root-template to childen templates like auth-template.yaml

--- deploy-scripts/auth-deployment.sh
+++ deploy-scripts/auth-deployment.sh
@@ -3,7 +3,7 @@
 mkdir "build"
 
 sam package --template ./meteo_pipe/auth/auth-template.yaml --s3-bucket meteopipe-app --s3-prefix auth-setting \
- --region eu-central-1 --output-template-file ./build/auth-template-out.yaml
+  --region eu-central-1 --output-template-file ./build/auth-template-out.yaml
 
 sam deploy --template-file ./build/auth-template-out.yaml \
-  --stack-name meteo-pipe --capabilities CAPABILITY_NAMED_IAM
+  --stack-name auth-meteo-pipe --capabilities CAPABILITY_NAMED_IAM
--- deploy-scripts/package-root-deployment.sh
+++ deploy-scripts/package-root-deployment.sh
@@ -3,7 +3,7 @@
 mkdir "build"
 
 sam package --template ./root-template.yaml --s3-bucket meteopipe-app --s3-prefix root-setting \
- --region eu-central-1 --output-template-file ./build/root-template-out.yaml
+  --region eu-central-1 --output-template-file ./build/root-template-out.yaml
 
 sam deploy --template-file ./build/root-template-out.yaml \
-  --stack-name auth-meteo-pipe --capabilities CAPABILITY_NAMED_IAM
+  --stack-name meteo-pipe --capabilities CAPABILITY_NAMED_IAM
