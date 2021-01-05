4b31c542411d18f1e43a052612d8107107b3259e
rozek1997 <marek.roskowicz@student.wat.edu.pl>
2021-01-05 21:19:43 +0100

    add dynamodb table for devices in cloudformation script

--- deploy-scripts/clean-deployment.sh
+++ deploy-scripts/clean-deployment.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 aws cloudformation delete-stack --stack-name "auth-meteo-pipe"
-sleep 5
+aws cloudformation delete-stack --stack-name "api-meteo-pipe"
+sleep 10
 aws cloudformation delete-stack --stack-name "meteo-pipe"
