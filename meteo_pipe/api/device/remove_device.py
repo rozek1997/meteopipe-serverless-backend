ab2ee318e9b55be8fead49740aa3dad5d4c40616
rozek1997 <marek.roskowicz@student.wat.edu.pl>
2021-01-12 21:47:19 +0100

    updated remove device lambda with also removing policy

--- meteo_pipe/api/device/remove_device.py
+++ meteo_pipe/api/device/remove_device.py
@@ -7,11 +7,14 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 iot_client = boto3.client("iot")
+POLICY_TEMPLATE = "meteopipe-user-policy-{}"
+user_uuid = None
 
 
 def lambda_handler(event, context):
+    global user_uuid
     deviceId = event["queryStringParameters"]["deviceId"]
-
+    user_uuid = event["requestContext"]["authorizer"]["jwt"]["claims"]["sub"]
     logger.info("Event {}".format(event))
     answer = {"headers": "Content-Type': 'application/json"}
     try:
@@ -30,7 +33,22 @@ def lambda_handler(event, context):
 
 def delete_thing_from_iotcore(thing_name: str):
     logger.info("removing device with complete name {}".format(thing_name))
+    principals = get_thing_principales(thing_name)
+    detach_user_policy(thing_name, principals)
+    detach_cert(thing_name, principals)
+    make_cert_inactive(principals)
+    delete_cert(principals)
     iot_client.delete_thing_from_iotcore(thingName=thing_name)
+
+
+def detach_user_policy(thing_name, principals: []):
+    policy = POLICY_TEMPLATE.format(user_uuid)
+
+    for principal in principals:
+        iot_client.detach_policy(
+            policyName=policy,
+            target=principal
+        )
 
 
 def get_thing_principales(thing_name: str):
@@ -41,12 +59,29 @@ def get_thing_principales(thing_name: str):
 
 
 def detach_cert(thingName: str, principals: []):
-    pass
+    for principal in principals:
+        iot_client.detach_thing_principal(
+            thingName=thingName,
+            principal=principal
+        )
+
+
+def get_cert_id_from_arn(certificate_arn: str):
+    certificateId = certificate_arn.rsplit("/", 1)[-1]
+    logger.info(certificateId)
+    return certificateId
 
 
 def make_cert_inactive(principals: []):
-    pass
+    for principal in principals:
+        iot_client.update_certificate(
+            certificateId=get_cert_id_from_arn(principal),
+            newStatus="INACTIVE"
+        )
 
 
-def delete_cert():
-    pass
+def delete_cert(principals: []):
+    for principal in principals:
+        iot_client.delete_certificate(
+            certificateId=get_cert_id_from_arn(principal),
+        )
